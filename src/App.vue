ace42b7b329a28ebbd69cd64f6b4fa7c4d5b722e
Bigsongyao <824196182@qq.com>
2019-01-07 23:24:02 +0800

    测试测试3

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,9 @@ export default {
 <style>
   @import '/static/css/element-ui.css';
   @import '/static/css/base.css';
-  @import '/static/css/style.css';
+  @import '/static/css/style王王王王王王王王.css';
 
   /* @import '/addons/sunny_storev1/plugin/kbaindex/template/mobile/static/css/element-ui.css';
   @import '/addons/sunny_storev1/plugin/kbaindex/template/mobile/static/css/base.css';
-  @import '/addons/sunny_storev1/plugin/kbaindex/template/mobile/static/css/style.css'; ffffffffffff*/
+  @import '/addons/sunny_storev1/plugin/kbaindex/template/mobile/static/css/style.css'; */
 </style>
